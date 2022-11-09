e79618a30c7bc86b94d662a52a2e42f4b96ff030
darricklaidin <darricklaidin@gmail.com>
2022-11-09 22:46:35 +1100

    Build CV Education section

--- src/components/CVEducation.js
+++ src/components/CVEducation.js
@@ -2,26 +2,33 @@ import React, { Component } from "react";
 import style from "../css/CVEducation.module.css";
 
 class CVEducation extends Component {
-  constructor() {
-    super();
-  }
   
   render() {
+    const { educations } = this.props;
+    
     return (
       <div className={style.wrapper}>
         <h3>
           Education
         </h3>
-        <div className={style.info}>
-          <div className={style.content}>
-            <p>RMIT University</p>
-            <p>Bachelors of Software Engineering</p>
-            <p>20 November 2022 - 30 December 2023</p>
-          </div>
-          <div className={style.location}>
-            <p>Melbourne, AU</p>
-          </div>
+        
+        <div className={style.infoWrapper}>
+          {educations.map((education) => {
+            return (
+              <div className={style.info} key={education.id}>
+                <div className={style.content}>
+                  <p>{education.university}</p>
+                  <p>{education.degree}</p>
+                  <p>{education.startDate} - {education.endDate}</p>
+                </div>
+                <div className={style.location}>
+                  <p>{education.city}, {education.country}</p>
+                </div>
+              </div>
+            )
+          })}
         </div>
+        
       </div>
     )
   }
